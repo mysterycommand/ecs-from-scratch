4c81a6305960dbea74c6a6381c6b8f65025ee427
Matt Hayes <matt@mysterycommand.com>
2019-10-27 09:07:00 -0400

    chore(src/main): spike out a `fn.toString` experiment

--- src/main.ts
+++ src/main.ts
@@ -1,4 +1,15 @@
 import './main.css';
 import { on } from './lib';
 
-on(window, 'load', () => {});
+on(window, 'load', () => {
+  [
+    (life: { value: number }) => {
+      life.value -= 1;
+    },
+    ([position, velocity]: { value: number }[]) => {
+      position.value += velocity.value;
+    },
+  ].forEach((fn: Function) => {
+    console.log(fn.toString());
+  });
+});
