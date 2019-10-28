9057a710ff96d0944d55d965fd5f501a9e8b3368
Matt Hayes <matt@mysterycommand.com>
2019-10-27 20:11:44 -0400

    feat(spike/main): fucking around with getting argument names from function string

--- src/main.ts
+++ src/main.ts
@@ -2,14 +2,45 @@ import './main.css';
 import { on } from './lib';
 
 on(window, 'load', () => {
+  const stripComments = /(?:(?:\/\/.*$)|(?:\/\*[\s\S]*?\*\/))/gm;
+  const endOfArguments = /\ =>|\ \{/;
+  const emptySpace = /\n*\s*/gm;
+
   [
-    (life: { value: number }) => {
-      life.value -= 1;
+    (foo: { value: number }) => {
+      foo.value -= 1;
     },
-    ([position, velocity]: { value: number }[]) => {
-      position.value += velocity.value;
+    ([bar, baz /* , qux */, plz]: { value: number }[]) => {
+      // hi
+      /**
+       * hello
+       */
+      bar.value += baz.value;
+      baz.value += plz.value;
+    },
+    function hello(hi: any /* how are you? */) {
+      // good?
+      console.log(hi);
+    },
+    function({ target, currentTarget }: Event) {
+      console.log(target, currentTarget);
+    },
+    function({ target, touches: [{ screenX, screenY }] }: TouchEvent) {
+      console.log(target, screenX, screenY);
+    },
+    function([{ a, b }]: { a: any; b: any }[]) {
+      console.log(a, b);
     },
   ].forEach((fn: Function) => {
-    console.log(fn.toString());
+    const functionString = fn
+      .toString()
+      .replace(stripComments, '')
+      .replace(`function ${fn.name}`, '');
+    const { index } = functionString.match(endOfArguments) || { index: 0 };
+    const argumentsString = functionString
+      .slice(0, index)
+      .replace(emptySpace, '');
+
+    console.log(argumentsString);
   });
 });
