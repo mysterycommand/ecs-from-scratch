10761ce6875d563aa8f5e74114a1dc25d7587b2e
Matt Hayes <matt@mysterycommand.com>
2019-10-29 07:45:31 -0400

    feat(unity): 02. Moving the Player
    @see:
    https://learn.unity.com/tutorial/environment-and-player?projectId=5c51479fedbc2a001fd5bb9f#5c7f8529edbc2a002053b786

---
+++ Unity/Roll a Ball/Assets/Scripts/PlayerController.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    public float speed = 1f;
+    private Rigidbody rb;
+
+    void Start()
+    {
+        rb = GetComponent<Rigidbody>();
+    }
+
+    void FixedUpdate()
+    {
+        float vertical = Input.GetAxis("Vertical");
+        float horizontal = Input.GetAxis("Horizontal");
+
+        Vector3 movement = new Vector3(horizontal, 0f, vertical);
+        rb.AddForce(movement * speed);
+    }
+}
