7d5822eda7db7c77c83b60651692855cf3c7950f
Matt Hayes <matt@mysterycommand.com>
2019-11-10 17:48:07 -0500

    refactor(components): fucking around with components, trying to figure out an interface/storage

--- src/main.ts
+++ src/main.ts
@@ -1,23 +1,48 @@
 import './main.css';
-import { on, createEntityPool } from './lib';
+import { on, createEntityPool, raf } from './lib';
 
-const [entities, request, restore] = createEntityPool();
+const [entities, request /* , restore */] = createEntityPool();
+
+type Data = number | { [key: string]: number };
+type Components = { [name: string]: Data };
+
+const componentStore: Map<number, Components> = new Map();
+function addComponents(entity: number, components: Components) {
+  componentStore.set(entity, components);
+}
+
+const { PI: π, sin, cos, random } = Math;
+const ππ = π * 2;
 
 on(window, 'load', () => {
-  request(4);
-  restore(0);
-  restore(2);
-  request(3);
-  request();
-  console.log([...entities], entities.currentSize, entities.totalSize);
-  restore(3);
-  restore(1);
-  console.log([...entities], entities.currentSize, entities.totalSize);
-  console.log(request(2));
-  console.log(request(2));
-  console.log([...entities], entities.currentSize, entities.totalSize);
-  restore([1, 3]);
-  console.log([...entities], entities.currentSize, entities.totalSize);
-  restore([0, 2]);
-  console.log([...entities], entities.currentSize, entities.totalSize);
+  request(20).forEach(entity => {
+    const a = random() * ππ;
+    const x = sin(a);
+    const y = cos(a);
+
+    addComponents(entity, {
+      position: { x: 0, y: 0 },
+      velocity: { x, y },
+      age: 0,
+    });
+  });
+
+  let firstTime = performance.now();
+  let previousTime = firstTime;
+  let normalTime = 0;
+  let deltaTime = 0;
+
+  raf(function tick(time: DOMHighResTimeStamp) {
+    raf(tick);
+
+    normalTime = time - firstTime;
+    deltaTime = normalTime - previousTime;
+    console.log(deltaTime);
+
+    [...entities].forEach(entity => {
+      (componentStore.get(entity)!.age as number) += deltaTime;
+    });
+
+    previousTime = normalTime;
+  });
 });
